31b24aa0baf3312264db01c25e9bee9885e79fc5
kyo4488 <kyokyo4816@gmail.com>
2025-01-13 21:54:23 +0900

    feat: summarize関数の統合

--- src/summarize.py
+++ src/summarize.py
@@ -1,5 +1,7 @@
 import openai
 import os
+import requests
+import json
 
 from dotenv import load_dotenv
 from langchain_openai import OpenAIEmbeddings
@@ -7,6 +9,8 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma, FAISS
 from langchain.chains import RetrievalQA
 from langchain_openai import ChatOpenAI
+
+from scraper import scrape_book_content
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -16,53 +20,20 @@ model = "gpt-4o"
 
 book_path = "./data/book.txt"
 
-
 def extract_book(path: str):
     with open(path, "r") as file:
         return file.read()
+    
+def get_push_count():
+    try:
+        with open("push_count.json", "r") as f:
+            data = json.load(f)
+            return data["push_count"]
+    except (FileNotFoundError, json.JSONDecodeError):
+        return 0
 
 
-def summarize(push_count=5):
-    # テキストの読み込み
-    book_text = extract_book(book_path)
-    # チャンクの設定
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1500,
-        chunk_overlap=200
-    )
-
-    # RAG用のドキュメントを作成
-    documents = text_splitter.create_documents([book_text])
-
-    # 埋め込みの作成
-    embeddings = OpenAIEmbeddings(model=embedding_model)
-    vectorstore = Chroma.from_documents(documents, embedding=embeddings)
-
-    # 検索の設定
-    qa = RetrievalQA.from_chain_type(
-        llm=ChatOpenAI(model_name=model, temperature=0),
-        chain_type="stuff",
-        retriever=vectorstore.as_retriever(search_kwargs={"k": 5}),
-        return_source_documents=False
-    )
-
-    if push_count <= 2:
-        query = "与えられた本の内容を短めに要約してください。"
-    elif push_count<= 4:
-        query = "与えられた本の内容を要約してください。"
-    else:
-        query = "与えられた本の内容を長めに要約してください。"
-        
-    print(query)
-    output = qa.invoke(query)
-
-    print("Summary of the Book:")
-    print(output["result"])
-
-
-# webからのテスト用
-def summarize_from_web(book_text):
-
+def summarize(book_text):
     # チャンクの設定
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=200)
 
@@ -85,9 +56,16 @@ def summarize_from_web(book_text):
         retriever=vectorstore.as_retriever(search_kwargs={"k": 5}),
         return_source_documents=False,
     )
+    push_count = get_push_count()
+    print("push_count:", push_count)
 
-    query = "与えられた本の内容を要約してください。"
-
+    if push_count <= 2:
+        query = "与えられた本の内容を短めに要約してください。"
+    elif push_count<= 4:
+        query = "与えられた本の内容を要約してください。"
+    else:
+        query = "与えられた本の内容を長めに要約してください。"
+        
     print(query)
     output = qa.invoke(query)
 
